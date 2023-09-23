c744d5ff79745bd44b1bf399e1bbc3179a272104
Your Name <mikachupichu@gmail.com>
2023-09-23 17:32:53 +0300

    Adding TaxRates function

---
+++ Salary/salary.js
@@ -0,0 +1,10 @@
+// Array consisting of the tax rates.
+const TaxRates = [
+    {MaxS:24000,rate:0.1},
+    {MaxS:32333,rate:0.25},
+    {MaxS:500000,rate:0.3},
+    {MaxS:800000,rate:0.325},
+    {MaxS:Infinity,rate:0.35}
+];
+
+

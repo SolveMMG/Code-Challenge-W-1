115f730c36027acb1bda17a32a2d39833637b1f6
Your Name <mikachupichu@gmail.com>
2023-09-23 17:33:34 +0300

    Adding NHif rates function

--- Salary/salary.js
+++ Salary/salary.js
@@ -7,4 +7,30 @@ const TaxRates = [
     {MaxS:Infinity,rate:0.35}
 ];
 
+//Array consisting of NHIF rates.
+
+const NHIFrates= [
+    {MaxS:5999,deduction:150},
+    {MaxS:7999,deduction:300},
+    {MaxS:11999,deduction:400},
+    {MaxS:14999,deduction:500},
+    {MaxS:19999,deduction:600},
+    {MaxS:24999,deduction:750},
+    {MaxS:29999,deduction:850},
+    {MaxS:34999,deduction:900},
+    {MaxS:39999,deduction:950},
+    {MaxS:44999,deduction:1000},
+    {MaxS:49999,deduction:1100},
+    {MaxS:59999,deduction:1200},
+    {MaxS:69999,deduction:1300},
+    {MaxS:79999,deduction:1400},
+    {MaxS:89999,deduction:1500},
+    {MaxS:99999,deduction:1600},
+    {MaxS:Infinity,deduction:11700},
+];
+
+
+
+
+
 
